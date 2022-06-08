5b91e4aaafcea32af8ef00587627fc64812e348f
Jose Edwin <edwinguaza951@gmail.com>
2022-06-08 10:03:15 -0500

    HE CONSTRUIDO EL SONIDO DEL JUEGO

--- flappyBird.js
+++ flappyBird.js
@@ -12,3 +12,23 @@ let tituloPuntaje = document.querySelector(".score_title");
 let logoReturn = document.querySelector(".logoReturn");
 let mensaje2 = document.querySelector(".mensaje2");
 let estadoJuego = "Start";
+
+let audio = new Audio("audio.mp3");
+audio.play();
+audio.loop = false;
+audio.playbackRate = 2;
+
+let inpacto = new Audio("inpacto.mp3");
+inpacto.loop = false;
+inpacto.playbackRate = 2;
+inpacto.play();
+
+let gameover = new Audio("gameOver.mp3");
+gameover.loop = false;
+gameover.playbackRate = 2;
+gameover.play();
+
+let finJuego = new Audio("007741506_prev.mp3");
+finJuego.loop = false;
+finJuego.playbackRate = 2;
+finJuego.play();
